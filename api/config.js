d6d614768eb711e0a6fe4f521f5f4cd76bc05875
younesmoulila <younes.moulila@gmail.com>
2023-12-09 12:47:32 +0100

    test

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'projet',
-    dbPath: 'C:/Users/Younes/Downloads/web2-2023-project-group-3/web2-2023-project-group-3/sushiRoyal.db', // Change for your own database file path
+    dbPath: '', // Change for your own database file path
 };
